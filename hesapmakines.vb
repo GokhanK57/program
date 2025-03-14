5067885e2b2bffb99e333353d0ec35591ef0b606
Gökhan <gokhankaragulle571@gmail.com>
2025-03-14 20:44:48 +0300

    Update hesapmakines.vb

--- hesapmakines.vb
+++ hesapmakines.vb
@@ -1,6 +1,7 @@
 Public Class Form1
     'Form şekli nasıl olmalı hakkında bir fotoğraf bulunmaktadır
     Dim ilksayi, ikincisayi, sonsayi As Double 'Bunlar bizim tanımlamalarımız, evrensel tanımlamalar için Public Class içine, belirli bir grup için tanımlama ise Private Sub altına yazılarak yapılır
+    'As Double yazarak bu tanımlamaları bir sayısal değer olarak atamış oldum
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         ilksayi = Val(TextBox1.Text) 'Formumuz üzerinde TextBox var ve kullanıcı buraya bir şey yazdığında onun sayısal değerini almasına yarar ve ilksayi'ya eşitler
         ikincisayi = Val(TextBox2.Text) 'Formumuz üzerinde TextBox var ve kullanıcı buraya bir şey yazdığında onun sayısal değerini almasına yarar ve ikincisayi'ya eşitler
